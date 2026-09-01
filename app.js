6f5d6ab8b6f354acea3bbe9ef0b574f3f50b5d5d
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-01 22:58:33 +0100

    Update project

--- app.js
+++ app.js
@@ -1,31 +1,28 @@
 const express = require('express');
-const morgan =  require('morgan');
+const morgan = require('morgan');
 
- const tourRouter = require('./routes/tourRoutes');
- const userRouter = require('./routes/userRoutes');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-
-const app =  express();
+const app = express();
 
 // 1) Middlewares
-app.use(morgan('dev'));
-app.use(express.json())
 
-app.use((req, res, next) => {
-    console.log('Hello from the middleware 👌');
-    next();
-})
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
-app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-})
- 
+app.use(express.json());
 
-    // Mount Router
-    app.use('/api/v1/tours', tourRouter)
-    app.use('/api/v1/users', userRouter)
+app.use(express.static(`${__dirname}/public`));
 
-    // Start Server
 
-    module.exports = app
+// 2) Mount Routers
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+
+// 3) Export app
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const app = require('./app')
+const dotenv = require('dotenv');
 
+dotenv.config({ path: './config.env' });
 
-const port = 3000
-app.listen(port , () => {
-    console.log(`App running on port ${port}....`)
-})
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}....`);
+});
