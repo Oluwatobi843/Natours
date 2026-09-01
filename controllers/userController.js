7536a6e16a6e8e06daca002591df8b7289ee5e60
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-01 10:34:18 +0100

    Extract controllers for tours & users

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet define'
+    })
+}
+
+exports.createUser = (req, res) => {
+     res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet define'
+    })
+}
+
+exports.getUser = (req, res) => {
+
+}
+
+exports.updateUser = (req, res) => {
+
+}
+
+exports.deleteUser = (req, res) => {
+
+}
