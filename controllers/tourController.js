a84411950438adaccd57c4c44b8d51f8443b8b31
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-04 13:49:40 +0100

    Expand tour schema and error handling

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -60,22 +60,20 @@ exports.createTour = async (req, res) => {
         }
     })
    
-
-
   } catch(err){
     res.status(400).json({
         status: 'fail',
-        message: 'Invalid data sent'
+        message: err
     })      
   }
      
 }
 
-
+   
 exports.updateTour = async (req, res) => {
     try{
 
-   await Tour.findByIdAndUpdate(req.params.id, req.body, {
+   const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
         new: true, 
         runValidators: true
     })        
@@ -84,18 +82,29 @@ exports.updateTour = async (req, res) => {
         status: 'success',
         data: {
             tour
-        }
+        } 
     })
     }catch (err){
-
+          res.status(400).json({
+        status: 'fail',
+        message: err
+    })   
     }
 } 
 
-exports.deleteTour  =  (req, res) => {
+exports.deleteTour  =  async (req, res) => {
+   try{
 
+         await Tour.findByIdAndDelete(req.params.id)
 
-    res.status(204).json({
+     res.status(204).json({
         status: 'success',
-        data: null
+        data: null   
     });
-}
+   }catch (err){
+        res.status(400).json({
+        status: 'fail',
+        message: err
+    })   
+   }
+}       
--- models/tourModel.js
+++ models/tourModel.js
@@ -5,6 +5,32 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a name']
     },
+
+    duration: {
+        type: Number,
+        required: [true, 'A tou must have  a duration']
+    },
+
+    maxGroupSize: {
+        type: Number, 
+        required: [true, 'A tour must have a group size']
+    },
+
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+
+    ratingsAverage: {
+        type: Number,
+        default: 4.5
+    },
+
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+
     rating: {
         type: Number,
         default: 4.5
@@ -12,7 +38,32 @@ const tourSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description']
+    }, 
+
+    description: {
+        type: String,
+        trim: true
+    },
+
+    imageCover: {
+        type: String,
+        required: [true, 'Á tour must have a cover image ']
+    },
+
+    images: [String],
+    createAt: {
+        type: Date,
+        default: Date.now()
+    },
+
+    startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
