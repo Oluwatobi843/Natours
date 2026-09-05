cd8d59a2ab6eba8b902e0198353469cca534e204
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-05 11:58:45 +0100

    update

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -8,18 +8,30 @@ const Tour = require('./../models/tourModel')
 
 exports.getAllTours = async (req, res) => {
     try{
-        
-    const tours = await Tour.find();
 
+        // BUILD QUERY
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach(el => delete queryObj[el])
+
+     
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
        status: 'success',
        results: tours.length, 
        data: {
         tours       
        }
-    })
+    }) 
     }catch (err){
-        res.status(400).json({
+        res.status(400).json({ 
             status: 'fail',
             message: err
         })
