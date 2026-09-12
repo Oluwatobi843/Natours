1f301e0cd1a61fb1261a62b4f683185778d35f5d
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-12 12:04:20 +0100

    Update Natours project and remove node_modules

--- app.js
+++ app.js
@@ -20,8 +20,15 @@ app.use(express.static(`${__dirname}/public`));
 // 2) Mount Routers
 
 app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);    
 
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+})
 
 // 3) Export app
 
