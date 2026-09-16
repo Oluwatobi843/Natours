c742a0d106ee1accecaa84a50e381f54a4004c74
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-16 20:56:48 +0100

    Add user Mongoose model
    Add new models/userModel.js which defines and exports a User Mongoose model. Schema includes name, email, photo, password, and passwordConfirm with basic validations (required fields, email uniqueness and format via validator.isEmail, lowercase email, password minlength).

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please tell us your name'],
+        // validate: [validator.isAlpha, 'Tour name must only contain characters']       
+    },
+
+    email: {
+        type: String,
+        required: [true, 'Please provide your email'],
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valid email']
+    },
+
+    photo: {
+        type: String,
+       
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8 
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: 8 
+    },
+}) ;
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;   
