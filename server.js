0c78ff89695f9ff76539e37f161d4f8047bdc2e5
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-22 07:19:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 
-
 process.on('uncaughtException', err => {
     console.log('UNCAUGHT EXCEPTION! Shutting down');
     console.log(err.name, err.message);
     server.close(() => {
         process.exit(1);
-    })
-  
+    }) 
 })
 dotenv.config({ path: './config.env' });
 const app = require('./app');
-
- 
 
 const DB =  process.env.DATABASE.replace(
     '<PASSWORD>',
@@ -25,15 +21,12 @@ mongoose.connect(DB, {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-}).then(() => console.log('DB connection successfull'))
-
- 
+}).then(() => console.log('DB connection successfull')) 
 const port = process.env.PORT || 3000;      
 
 const server = app.listen(port, () => {      
     console.log(`App running on port ${port}....`);
 });    
-
 process.on('unhandledRejection', err => {
     console.log(err.name, err.message);
     console.log('UNHANDLER REJECTION! Shutting down');
