bca55bc7bdbaa59192f351ecd8ef2885428a946e
Oluwatobi843 <alaooluwatobi97@gmail.com>
2026-09-23 22:57:55 +0100

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,21 @@
+
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+
 const express = require('express');
 
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet define'
-    })
-}
+exports.getAllUsers = catchAsync( async (req, res, next) => {
+    const users = await User.find()
+
+    // SEND RESPONSE
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
+    });
+});     
 
 exports.createUser = (req, res) => {
      res.status(500).json({
